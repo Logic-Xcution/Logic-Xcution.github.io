b3246958c3efa2a3a53702331a55b01633b8687a
Madhav Bahl <madhavbahl10@gmail.com>
2018-04-22 11:13:15 +0530

    Change launch date

--- js/mainT.js
+++ js/mainT.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "May 15, 2018 10:00:00",
+	date: "May 30, 2018 10:00:00",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
