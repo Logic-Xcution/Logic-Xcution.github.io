6c974f50c4480ed0d439758df599c78fb18f8de0
Madhav Bahl <madhavbahl10@gmail.com>
2018-06-02 16:59:03 +0530

    Extend Website Date

--- js/mainT.js
+++ js/mainT.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "May 30, 2018 10:00:00",
+	date: "June 30, 2018 10:00:00",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
